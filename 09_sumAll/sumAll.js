5713a4dd5c043498678174770df22f99d0742e88
Bafikafi <Bafikafi37@gmail.com>
2025-12-04 21:58:22 +0100

    Finished excersises 9-11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b) ||
+    !(typeof a === "number" || typeof b === "number")
+  )
+    return "ERROR";
+  let aa = a < b ? a : b;
+  let bb = b > a ? b : a;
+  let sum = 0;
+  for (i = aa; i <= bb; i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
