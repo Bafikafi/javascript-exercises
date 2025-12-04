e5aebece9fde5a1cf8e2e4418682b3d61c30d4c0
Bafikafi <Bafikafi37@gmail.com>
2025-12-04 21:20:35 +0100

    Completed excercises 6-8

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+/**@param {String} string */
+const reverseString = function(string) {
+    let s = ''
+    for (let i = string.length; i >=0; i--){
+        s += string.charAt(i)
+    }
+    return s
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {Array} arr 
+ * 
+ */
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1)
+    return arr.filter(a => !args.includes(a))
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
